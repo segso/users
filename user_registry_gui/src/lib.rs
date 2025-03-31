c3147a13870f767d05f37afb238d3f79a38f7a59
SEG <segdev.mx@gmail.com>
2025-03-30 18:16:30 -0600

    feat(gui): initialize state with data file in run function

--- user_registry_gui/src/lib.rs
+++ user_registry_gui/src/lib.rs
@@ -7,11 +7,16 @@ mod state;
 
 use app::App;
 pub use error::Error;
+use state::State;
+
+use iced::Task;
 
 use std::path::Path;
 
-pub fn run<P: AsRef<Path>>(_data_file: P) -> Result<(), Error> {
-    iced::run(App::title, App::update, App::view)?;
+pub fn run<P: AsRef<Path>>(data_file: P) -> Result<(), Error> {
+    let state = State::with_data_file(data_file)?;
+
+    iced::application(App::title, App::update, App::view).run_with(|| (state, Task::none()))?;
 
     Ok(())
 }
--- user_registry_gui/src/state.rs
+++ user_registry_gui/src/state.rs
@@ -1,6 +1,22 @@
+use std::{io, path::Path};
+
+use user_registry_lib::{Data, command::read_data};
+
 use crate::page::Page;
 
 #[derive(Default)]
 pub struct State {
     pub page: Page,
+    pub data: Data,
+}
+
+impl State {
+    pub fn with_data_file<P: AsRef<Path>>(data_file: P) -> Result<Self, io::Error> {
+        let data = read_data(data_file)?;
+
+        Ok(Self {
+            data,
+            ..Default::default()
+        })
+    }
 }
